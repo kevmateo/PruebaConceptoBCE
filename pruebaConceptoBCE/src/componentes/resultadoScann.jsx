d887d739410f4b95315bbe10e3c13ebeb3c9babd
kevmateo <117870690+kevmateo@users.noreply.github.com>
2024-09-17 17:40:42 +0000

    arreglos

--- pruebaConceptoBCE/src/componentes/resultadoScann.jsx
+++ pruebaConceptoBCE/src/componentes/resultadoScann.jsx
@@ -3,15 +3,14 @@ import Button from 'react-bootstrap/Button';
 
 function ResultadoScann({ beneficiario, entidadFinanciera, numCuenta }) {
   return (
-    <div className="flex flex-col p-3 items-cemter justify-center rounded-[10px]">
-      <section className="flex flex-col items-center justify-center w-screen border p-5">
+    <div className="flex flex-col p-3 items-cemter justify-center">
+      <section className="flex flex-col items-center justify-center w-screen p-5">
         <span className="text-center my-1 font-bold text-xl">{beneficiario}</span>
         <span className="text-center my-1">{entidadFinanciera}</span>
         <span className="text-center my-1">Cuenta Nro. {numCuenta}</span>
         <Form.Control type="number" placeholder="$ 0.00" />
-
       </section>
-      <section className="flex rounded-[10px] border-[#D9D9D9] p-5 items-center justify-between bg-red-200" >
+      <section className="flex rounded-[10px] border-[#D9D9D9] p-3 items-center justify-center border mx-5 gap-5" >
         <div className="flex flex-col">
           <span>Desde</span>
           <span>KEVIN</span>
@@ -19,7 +18,7 @@ function ResultadoScann({ beneficiario, entidadFinanciera, numCuenta }) {
         </div>
         <span>$30.00</span>
       </section>
-      <Button variant="primary" className="mt-2">Transferir</Button>
+      <Button variant="primary" type="submit" className="mt-2 mx-5">Transferir</Button>
     </div>
   )
 }
